447d2d211f413fd607842159fb85761bcb641e12
Susu <susu@minimuutti.com>
2017-07-13 17:03:42 +0300

    update

--- en/brain-games/_posts/insert-redirect.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for FILE in $(ls); do
-	NAME=$(echo ${FILE} | perl -pe 's/\d+-\d+-\d+-(.+)\.md/\1/')
-	sed -i "0,/---/! {0,/---/ s/---/redirect_from:\n  - \/en\/activation\/${NAME}\n---/}" "${FILE}"
-done
